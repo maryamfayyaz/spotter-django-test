ee69839510974900b2331ca301a2ab6e13af2433
Maryam Fayyaz <maryamfayyaz1164@gmail.com>
2025-06-25 14:32:43 +0500

    allowed host added

--- fuel_route_api/settings.py
+++ fuel_route_api/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = "dummy-secret-key"
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['spotter-django-test.onrender.com']
 
 INSTALLED_APPS = [
     "django.contrib.admin",
